11ffdadb0855371867691fbe068b1dd2c43c6afa
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2020-07-31 01:59:25 -0400

    Added more elements and started building the structure

--- demo/exampleDatabase.js
+++ demo/exampleDatabase.js
@@ -1,6 +1,35 @@
 const exampleData = [
-    {
-        type: "h2",
-        value: "This Will Be a Header"
-    }
+    [
+        {
+            type: "h2",
+            value: "This Is a Big Header"
+        },
+        {
+            type: "h3",
+            value: "This Is a Small Header"
+        },
+        {
+            type: "ul",
+            value: ["Element 1", "Element 2", "Element 3", "Element 4"]
+        },
+        {
+            type: "p",
+            value: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima exercitationem similique tempore vero molestias officia consequuntur eum rerum neque autem nulla, corporis dolores! Similique ut molestiae vitae, reiciendis non dolorem.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima exercitationem similique tempore vero molestias officia consequuntur eum rerum neque autem nulla, corporis dolores! Similique ut molestiae vitae, reiciendis non dolorem."
+        }
+    ],
+    [
+        {
+            type: "h2",
+            value: "This Is a Big Header"
+        },
+        {
+            type: "h3",
+            value: "This Is a Small Header"
+        },
+        {
+            type: "ul",
+            value: ["Element 1", "Element 2", "Element 3", "Element 4"]
+        },
+
+    ]
 ]
--- dist/headlessCMS.js
+++ dist/headlessCMS.js
@@ -2,10 +2,28 @@ class HeadlessCMS {
     constructor(elementSelector, data = []) {
         this.elementSelector = elementSelector
         this.element = document.querySelector(elementSelector)
-        this.data = data
+        this.data = []
 
         this.initializeModule(data)
     }
+
+    importData(data) {
+        this.data = [...this.data, ...data]
+        data.forEach(section => {
+            const newSection = this.createSection()
+            section.forEach(element => {
+                newSection.appendChild(this.createElements(element))
+            })
+            this.element.appendChild(newSection)
+        });
+    }
+    exportData() {
+        return this.data
+    }
+
+
+
+
 
     initializeModule(data) {
         if (this.element == null) {
@@ -15,15 +33,24 @@ class HeadlessCMS {
         this.element.classList.add("headlessCMS-container")
         this.importData(data)
     }
-
-    importData(data) {
-        data.forEach(element => {
-            this.element.appendChild(this.createElement(element))
-        });
-    }
-    createElement(element) {
-        const node = document.createElement("h2")
+    createElements(element) {
+        const node = document.createElement(element.type)
         node.textContent = element.value
+
+        if (element.type === "ul" || element.type === "ol") {
+            node.textContent = ""
+            element.value.forEach(value => {
+                const li = document.createElement("li")
+                li.textContent = value
+                node.appendChild(li)
+            })
+        }
+
+        return node
+    }
+    createSection(){
+        const node = document.createElement("div")
+        node.classList.add("headlessCMS-section")
         return node
     }
 }
